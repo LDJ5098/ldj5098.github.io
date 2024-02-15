0e9e3d5b318488cbaf54b6de1e1698eac1621ebc
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2024-02-16 07:59:06 +0900

    Update main.js

--- main/main.js
+++ main/main.js
@@ -130,14 +130,26 @@ function ad_number(){
 
 
 // 이벤트 리스너 등록
-var inputElement = document.querySelector('#search_menu > input');
 var bottombar = document.getElementById('bottom_bar');
-inputElement.addEventListener('focus', function() {
-    console.log('Input에 포커스가 되었습니다.');
-    bottombar.style.position = 'absolute';
-});
 
-inputElement.addEventListener('blur', function() {
-    console.log('Input에서 포커스가 해제되었습니다.');
-    bottombar.style.position = 'fixed';
-});
+function detectKeyboardStatus() {
+    // 윈도우의 높이를 가져옴
+    var windowHeight = window.innerHeight;
+    
+    // 키보드가 활성화되었을 때 윈도우의 높이 변화를 감지
+    window.addEventListener('resize', function() {
+        var currentHeight = window.innerHeight;
+        
+        // 이전 윈도우의 높이와 현재 윈도우의 높이를 비교하여 키보드 상태를 판단
+        if (windowHeight > currentHeight) {
+            bottombar.style.position = 'absolute';
+        } else {
+            bottombar.style.position = 'fixed';
+        }
+    });
+}
+
+// 페이지 로드 시 키보드 상태 감지 함수 호출
+window.onload = function() {
+    detectKeyboardStatus();
+};
