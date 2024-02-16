4826a091c18a1cf60c0227ae7cd933cf990c3423
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2024-02-16 23:21:37 +0900

    Add files via upload

---
+++ bottom_bar/bottom_button.js
@@ -0,0 +1,12 @@
+///////////////////하단 메뉴바 이동제어////////////////////////////
+document.getElementById("category").addEventListener("click", function() {
+    window.location.href = "../community/community.html";
+});
+
+document.getElementById("home").addEventListener("click", function() {
+    window.location.href = "../main/main.html";
+});
+
+document.getElementById("my").addEventListener("click", function() {
+    window.location.href = "../my_sullink/my_sullink.html";
+});
---
+++ pot/pot.js
@@ -0,0 +1,24 @@
+// 이벤트 리스너 등록
+var bottombar = document.getElementById('bottom_bar');
+
+function detectKeyboardStatus() {
+    // 윈도우의 높이를 가져옴
+    var windowHeight = window.innerHeight;
+    
+    // 키보드가 활성화되었을 때 윈도우의 높이 변화를 감지
+    window.addEventListener('resize', function() {
+        var currentHeight = window.innerHeight;
+        
+        // 이전 윈도우의 높이와 현재 윈도우의 높이를 비교하여 키보드 상태를 판단
+        if (windowHeight > currentHeight) {
+            bottombar.style.position = 'absolute';
+        } else {
+            bottombar.style.position = 'fixed';
+        }
+    });
+}
+
+// 페이지 로드 시 키보드 상태 감지 함수 호출
+window.onload = function() {
+    detectKeyboardStatus();
+};
