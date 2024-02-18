80b563c7da4d94b7739a33a6008696be93163b85
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2024-02-18 17:13:53 +0900

    Update bottom_button.js

--- bottom_bar/bottom_button.js
+++ bottom_bar/bottom_button.js
@@ -1,8 +1,4 @@
 ///////////////////하단 메뉴바 이동제어////////////////////////////
-document.getElementById("category").addEventListener("click", function() {
-    window.location.href = "../community/community.html";
-});
-
 document.getElementById("home").addEventListener("click", function() {
     window.location.href = "../main/main.html";
 });
