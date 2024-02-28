a9c56f3e7dfeed529680c8585ac7af3fabad3bbd
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2024-02-28 21:48:13 +0900

    Add files via upload

---
+++ bottom_bar/bottom_button.js
@@ -0,0 +1,8 @@
+///////////////////하단 메뉴바 이동제어////////////////////////////
+document.getElementById("home").addEventListener("click", function() {
+    window.location.href = "../main/main.html";
+});
+
+document.getElementById("my").addEventListener("click", function() {
+    window.location.href = "../my_sullink/my_sullink.html";
+});
